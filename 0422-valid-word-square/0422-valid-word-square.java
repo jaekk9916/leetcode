487f90acd13ce9fc4dca1f266b90c8011590149f
Joy <96701717+jaekk9916@users.noreply.github.com>
2023-03-26 19:49:13 -0400

    Time: 13 ms (62.18%), Space: 42.4 MB (40.76%) - LeetHub

---
+++ 0422-valid-word-square/0422-valid-word-square.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean validWordSquare(List<String> words) {
+        boolean check = true;
+        int cols = words.size();
+        for(int i=0;i<words.size();i++){
+
+            String tmp = words.get(i);
+         
+            for(int j=0;j<tmp.length();j++){
+                if(j >= words.size() || i >= words.get(j).length() || tmp.charAt(j) != words.get(j).charAt(i)){
+                    return false;
+                }
+            }
+        }
+    return check;
+    }
+}

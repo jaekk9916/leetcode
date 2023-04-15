4ba41b2de6b0e69c164653b3f84396ebfedf3bb7
Joy <96701717+jaekk9916@users.noreply.github.com>
2023-04-15 13:45:51 -0400

    Time: 10 ms (53.73%), Space: 42.6 MB (14.59%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String str1 = Integer.toString(x);
-        char[] arrStr1 = str1.toCharArray();
-        char[] arrStr2 = new char[str1.length()];
+        String str = Integer.toString(x);
+        char[] arrStr = str.toCharArray();
         
-        for(int i=0;i<str1.length();i++){
-            if(arrStr1[i] != arrStr1[arrStr1.length-1-i])
+        for(int i=0;i<str.length();i++){
+            if(arrStr[i] != arrStr[arrStr.length-1-i])
             return false;
         }
         

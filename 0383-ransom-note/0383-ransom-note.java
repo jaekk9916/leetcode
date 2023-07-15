7e412f434b2b0f74ddf6abf0eaa68e8c963e073d
Joy <96701717+jaekk9916@users.noreply.github.com>
2023-07-15 15:31:15 -0400

    Time: 19 ms (45.41%), Space: 44.5 MB (9.36%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,28 +1,19 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> rMap = new HashMap<Character, Integer>();
         HashMap<Character, Integer> mMap = new HashMap<Character, Integer>();
         
-        for(int j = 0; j < magazine.length(); j++){
-            char character = magazine.charAt(j);
-            if(!mMap.containsKey(character)) mMap.put(character, 1);
-            else {
-            mMap.put(character, mMap.get(character) + 1);}
+        for(char c : magazine.toCharArray()){
+            int val = mMap.getOrDefault(c, 0);
+            mMap.put(c, val + 1);
         }
         
-        for(int i = 0; i < ransomNote.length(); i++){
-            char character = ransomNote.charAt(i);
-            if(!mMap.containsKey(character)){
+        for(char c : ransomNote.toCharArray()){
+            if(!mMap.containsKey(c) || mMap.get(c) == 0){
                 return false;
-            } else {
-               mMap.put(character, mMap.get(character) - 1); 
-            }
+            } 
             
-            if(mMap.get(character) == 0){
-                mMap.remove(character);
-            }
+            mMap.put(c, mMap.get(c) - 1);
         }
-        
         return true;
     }
 }

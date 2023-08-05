18c47f7490be3a161be0b62dd1c489c9cc9a95e4
Joy <96701717+jaekk9916@users.noreply.github.com>
2023-08-04 23:47:42 -0400

    Time: 46 ms (95.43%), Space: 42.2 MB (19.46%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -6,12 +6,12 @@ var expect = function(val) {
     return {
         toBe: (val2) => {
             if(val !== val2) throw new Error("Not Equal");
-            return true;
+            else return true;
         },
         
         notToBe: (val2) => {
             if(val === val2) throw new Error("Equal");
-            return true;
+            else return true;
         }
     }
 };

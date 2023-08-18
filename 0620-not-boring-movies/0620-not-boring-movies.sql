2e6fd11779274ed64db83959190e9a8c48e45d35
Joy <96701717+jaekk9916@users.noreply.github.com>
2023-08-18 11:32:43 -0400

    Time: 895 ms (29.47%), Space: 0B (100.00%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,6 +1,6 @@
 /* Write your PL/SQL query statement below */
 SELECT id, movie, description, rating
 FROM cinema
-WHERE description != 'boring' AND mod(id, 2) = 1 
+WHERE description <> 'boring' AND mod(id, 2) = 1 
 ORDER BY rating DESC
 ;
